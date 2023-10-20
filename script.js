ec88da3cb8b816fd3c6cbd369e5e1cd675e2a7ab
thishastobenoah <thishastobenoah@gmail.com>
2023-10-19 21:27:05 -0400

    fixed the counting issue and i scope

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-for(i=1; i <= 10; i++){
+for(let i=1; i <= 10; i++){
     console.log(i);
 }
 
-for(i=10; i >= 1; i--){
+for(let i=10; i >= 1; i--){
     console.log(i);
 }
 var i = 1;
-while (i <= 10){
+while (i < 10){
     console.log(i);
     i++;
 }
-var x = 10;
-while (x >= 1){
-    console.log(x);
-    x--;
+
+while (i > 1){
+    console.log(i);
+    i--;
 }
-var y=1;
 do{
-    console.log(y);
-    y++;
-}while(y<=10)
-var b = 10
+    console.log(i);
+    i++;
+}while(i<10)
 do{
-    console.log(b);
-    b--;
-}while(b>=10)
+    console.log(i);
+    i--;
+}while(i>10)
 
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for(num of numbers){
+for(let num of numbers){
     console.log(num);
 }
